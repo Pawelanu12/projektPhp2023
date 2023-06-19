8fd9466826a0468a878ee4ad75b581929deb0626
Paweł Anuszkiewicz <s26970@pjwstk.edu.pl>
2023-06-19 20:48:34 +0200

    Added script to removing single user in users management site.

--- website/management/remove_user.php
+++ website/management/remove_user.php
@@ -3,14 +3,29 @@
 
 	$dbConnection = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
+	$userId = $_GET["user-id"];
+
 	if($dbConnection)
 	{
 		
-		$dbRemoveUserQuery = 'DELETE FROM users WHERE user_id = 11;';
+		$dbRemoveUserQuery = 'DELETE FROM users WHERE user_id = '.$userId.';';
+
+		$dbRemoveUserQueryResult = mysqli_query($dbConnection, $dbRemoveUserQuery);
+
+		if($dbRemoveUserQueryResult)
+		{
+			echo("The user has been removed.");
+		}
+		else
+		{
+			echo("The user has been NOT removed.");
+		}
+		
+		echo('<br><br><a href="users_management.php">Return to users list</a><br>');
 		
 		mysqli_close($dbConnection);
 	}
-	echo
+	else
 	{
 		echo("ERROR: Connection to database WASN'T created.");
 	}
